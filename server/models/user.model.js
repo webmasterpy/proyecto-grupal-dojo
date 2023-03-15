4b60584168f062485c4d8fb6223431145b623352
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-14 23:34:54 -0300

    [Model] Modelo para el registro de usuario

---
+++ server/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+// * Creamos el esquema para nuestro modelo
+const userSchema = new mongoose.Schema({
+	email:{
+		type:String,
+		required: [true, "Please enter an email"],
+		unique: true,
+		lowercase: true,
+		validate:[isEmail, "Please enter a valid email"]
+	},
+	password:{
+		type: String,
+		required: [true, "Please enter a password"],
+		minlength: [6, "Minimun characters are 6"	]
+	},
+});
+
+// * Encriptar el password antes de guardar
+userSchema.pre("save", async function(next){
+	const salt = await bcrypt.genSalt();
+	this.password = await bcrypt.hash(this.password, salt);
+
+	next();
+})
+
+// * Método estático para que acceder por login
+userSchema.statics.login = async function(email, password) {
+	const user = await this.findOne({ email });
+
+	// Verificamos si existe el usuario
+	if(user){
+		const auth = await bcrypt.compare(password, user.password)
+		
+		if(auth)
+			return user;
+		
+		throw Error("Incorrect Password");
+	}
+	throw Error("Incorrect Email");
+}
+
+// * Creamos el modelo
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
