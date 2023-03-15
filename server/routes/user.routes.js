5e452852215eead6e2e9bdac708e528a60f0f8fb
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-15 15:52:11 -0300

    [server] Ruta de API para autenticar un usuario

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const authController = require("../controllers/user.controller");
+const { authToken } = require("../middleware/authToken")
 
 const router = Router();
 
 router.post("/api/signup", authController.signup_post);
 router.post("/api/login", authController.login_post);
 router.get("/api/logout", authController.logout_get);
+router.get("/api/authUser", authToken);
 
 module.exports = router;
