7234ebfe6481b94b4d1cc112f8063bddda0c4195
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-14 23:36:10 -0300

    [Routes] Rutas de acceso a la API para User

---
+++ server/routes/user.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const authController = require("../controllers/user.controller");
+
+const router = Router();
+
+router.post("/api/signup", authController.signup_post);
+router.post("/api/login", authController.login_post);
+router.get("/api/logout", authController.logout_get);
+
+module.exports = router;
